510656772905292f4a537fe0123e2d4e1f3a4ebe
ITeCK62 <yoplayzgaming@gmail.com>
2023-10-22 23:16:47 +0200

    Algo4 V0.1
    Création de liste chaînée + gestion des noeuds et affichage de la liste.

--- TP 4 Alog.c
+++ TP 4 Alog.c
@@ -5,6 +5,32 @@
  */
  
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Structure pour représenter un noeud dans une liste chaînée
+typedef struct Node {
+    // Chaîne de caractères représentant le mot
+    char word[50];
+    // Entier représentant le nombre d'occurrences du mot
+    int count;
+    // Pointeur vers le prochain noeud dans la liste chaînée
+    struct Node* next;
+} Node;
+
+// Fonction pour créer un nouveau noeud
+Node* createNode(char* word) {
+    // Allouer de la mémoire pour le nouveau noeud
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    // Copier le mot dans le nouveau noeud
+    strcpy(newNode->word, word);
+    // Initialiser le compteur du nouveau noeud à 1
+    newNode->count = 1;
+    // Initialiser le pointeur suivant du nouveau noeud à NULL
+    newNode->next = NULL;
+    // Retourner le nouveau noeud
+    return newNode;
+}
 
 // Fonction pour vérifier si une lettre est minuscule
 int lettreMin(char c){
@@ -77,6 +103,64 @@ int lectureFichier(){
     return 0;
 }
 
+// Fonction pour insérer un noeud dans la liste
+void insertNode(Node** head, char* word) {
+    // Si la liste est vide
+    if (*head == NULL) {
+        // Créer un nouveau nœud
+        *head = createNode(word);
+        return;
+    }
+
+    // Pointeur pour parcourir la liste
+    Node* current = *head;
+
+    // Tant que le noeud suivant n'est pas NULL
+    while (current->next != NULL) {
+        // Si le mot du noeud actuel est identique au mot à insérer
+        if (strcmp(current->word, word) == 0) {
+            // Incrémenter le compteur du noeud actuel
+            current->count++;
+            return;
+        }
+        // Passer au noeud suivant
+        current = current->next;
+    }
+
+    // Si le mot du noeud actuel est égal au mot à insérer
+    if (strcmp(current->word, word) == 0) {
+        // Incrémenter le compteur du noeud actuel
+        current->count++;
+        return;
+    }
+
+    // Si le mot à insérer est inférieur au mot du noeud actuel
+    if (strcmp(word, current->word) < 0) {
+        // Créer un nouveau noeud
+        Node* newNode = createNode(word);
+        // Le nouveau noeud pointe vers le noeud actuel
+        newNode->next = *head;
+        // Le noeud actuel pointe vers le nouveau noeud
+        *head = newNode;
+    } else {
+        // Le noeud actuel pointe vers le nouveau noeud
+        current->next = createNode(word);
+    }
+}
+
+// Fonction pour afficher la liste
+void printList(Node* head) {
+    // Pointeur pour parcourir la liste
+    Node* current = head;
+    // Tant que le pointeur actuel n'est pas NULL
+    while (current != NULL) {
+        // Afficher le mot et le compteur
+        printf("%s - %d\n", current->word, current->count);
+        // Passer au nœud suivant
+        current = current->next;
+    }
+}
+
 int main(){
     char c = 'e';
     /*lettreMin('z');
@@ -88,5 +172,12 @@ int main(){
         printf("Ce n'est pas une lettre\n");
     }
     convMinMaj(&c);*/
+    Node* head = NULL;
+    insertNode(&head, "banane");
+    insertNode(&head, "pomme");
+    insertNode(&head, "banane");
+    insertNode(&head, "orange");
+    insertNode(&head, "banane");
+    printList(head);
     return 0;
 }
